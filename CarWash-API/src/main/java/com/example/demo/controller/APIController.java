c459124501bdd4d93a0922de9ceac420d3fb69cc
Bhavya Prakash <bhavyaprakash@Bhavyas-MacBook-Air.local>
2022-08-19 18:55:59 +0530

    added server and controller in api

--- CarWash-API/src/main/java/com/example/demo/controller/APIController.java
+++ CarWash-API/src/main/java/com/example/demo/controller/APIController.java
@@ -1,5 +1,14 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class APIController {
+	
+	@RequestMapping("/car")
+	public String apiconnect() {
+		return "";
+	}
 
 }
