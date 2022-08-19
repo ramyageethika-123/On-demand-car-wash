95cb922d60308880c941758589439ff95d7225da
Ramya Geethika <iamramyageethika@gmail.com>
2022-08-19 23:18:15 +0530

    Orders-Microservice

---
+++ Orders_Service/src/main/java/com/example/demo/Controller/Controller.java
@@ -0,0 +1,13 @@
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	@RequestMapping("/hello")
+	public String Hello() {
+		return "Hello World 4";
+	}
+}
