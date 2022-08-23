a5994fda94d1e050df9160f5880ff3c97232fe92
Ramya Geethika <iamramyageethika@gmail.com>
2022-08-23 22:39:06 +0530

    Changed API

---
+++ Admin_Service/src/main/java/com/example/demo/entity/Admin.java
@@ -0,0 +1,67 @@
+package com.example.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "Admin" , schema="targetSchemaName")
+public class Admin {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "email")
+	private String email;
+
+	
+	
+	public Admin (String name, String email) {
+		this.name =name;
+		this.email=email;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
---
+++ Admin_Service/src/main/java/com/example/demo/service_a/Admin.java
@@ -0,0 +1,5 @@
+package com.example.demo.service_a;
+
+public class Admin {
+
+}
