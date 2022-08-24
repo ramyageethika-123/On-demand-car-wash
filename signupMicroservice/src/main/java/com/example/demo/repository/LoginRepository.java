4110bc64674148b808840e55000402aa5b6ffdc1
Bhavya Prakash <bhavyaprakash@Bhavyas-MacBook-Air.local>
2022-08-25 00:26:06 +0530

    removed not used model

--- signupMicroservice/src/main/java/com/example/demo/repository/LoginRepository.java
+++ signupMicroservice/src/main/java/com/example/demo/repository/LoginRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.Login;
-import com.example.demo.model.User;
 
 @Repository
 public interface LoginRepository extends JpaRepository<Login,Integer> {
--- signupMicroservice/src/main/java/com/example/demo/service/LoginService.java
+++ signupMicroservice/src/main/java/com/example/demo/service/LoginService.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.model.Login;
-import com.example.demo.model.User;
 import com.example.demo.repository.LoginRepository;
 
 @Service
